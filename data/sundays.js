af592a5478d112b12bef86d07a659ba6b9af2b28
fri <tehoko@gmail.com>
2019-11-14 19:56:16 +0100

    initial commit

---
+++ data/sundays.js
@@ -0,0 +1,594 @@
+function getSundays() {
+    return [{
+        "date": new Date("2019-11-03"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2019-11-10"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2019-11-17"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2019-11-24"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2019-12-01"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2019-12-08"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2019-12-15"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2019-12-22"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2019-12-29"),
+        "open": true,
+        "info": ""
+    },
+    // --- 2020 -----------------------------------------------
+    {
+        "date": new Date("2020-01-05"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-01-12"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-01-19"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-01-26"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-02-02"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-02-09"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-02-16"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-02-23"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-03-01"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-03-08"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-03-15"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-03-22"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-03-29"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-04-05"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-04-12"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-04-19"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-04-26"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-05-03"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-05-10"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-05-17"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-05-24"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-05-31"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-06-07"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-06-14"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-06-21"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-06-28"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-07-05"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-07-12"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-07-19"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-07-26"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-08-02"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-08-09"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-08-16"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-08-23"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-08-30"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-09-06"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-09-13"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-09-20"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-09-27"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-10-04"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-10-11"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-10-18"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-10-25"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-11-01"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-11-08"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-11-15"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-11-22"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-11-29"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-12-06"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-12-13"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-12-20"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2020-12-27"),
+        "open": false,
+        "info": ""
+    },
+    // --- 2021 -----------------------------------------------
+    {
+        "date": new Date("2021-01-03"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-01-10"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-01-17"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-01-24"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-01-31"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-02-07"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-02-14"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-02-21"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-02-28"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-03-07"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-03-14"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-03-21"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-03-28"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-04-04"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-04-11"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-04-18"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-04-25"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-05-02"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-05-09"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-05-16"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-05-23"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-05-30"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-06-06"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-06-13"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-06-20"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-06-27"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-07-04"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-07-11"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-07-18"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-07-25"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-08-01"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-08-08"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-08-15"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-08-22"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-08-29"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-09-05"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-09-12"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-09-19"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-09-26"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-10-03"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-10-10"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-10-17"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-10-24"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-10-31"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-11-07"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-11-14"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-11-21"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-11-28"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-12-05"),
+        "open": false,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-12-12"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-12-19"),
+        "open": true,
+        "info": ""
+    },
+    {
+        "date": new Date("2021-12-26"),
+        "open": false,
+        "info": ""
+    }];
+}
+
+/*
+wake me up when it's 2022
+https://www.kalbi.pl/niedziele-handlowe-2021
+
+function printSundays() {
+    let sundays = [];
+    let firstSunday = new Date("2021-12-26");
+    for (let i = 0; i < 200; i++) {
+        sundays.push(new Date(firstSunday.setDate(firstSunday.getDate() + 7)));
+    }
+    let out = "let sundays = [" + "\n";
+    for (let sd of sundays) {
+        out += '    {' + "\n";
+        out += '        "date": new Date("' + sd.toISOString().substring(0, 10) + '"),' + "\n";
+        out += '        "open": false,' + "\n";
+        out += '        "info": ""' + "\n";
+        out += '    },' + "\n";
+    }
+    out += '];'
+    return out;
+}
+
+console.log(printSundays());
+*/
---
+++ js/app.js
@@ -0,0 +1,298 @@
+function applyCss(rules) {
+    let styleElem = document.createElement("style");
+    styleElem.type = "text/css";
+    if (styleElem.styleSheet) styleElem.styleSheet.cssText = rules;
+    else styleElem.innerHTML = rules;
+    document.head.appendChild(styleElem);
+}
+
+function insertDomBefore(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode)
+}
+
+function insertDomAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function roundToTwo(number) {
+    return +(Math.round(number + "e+2") + "e-2");
+}
+
+function getClassForSunday(day) {
+    const sundays = getSundays();
+    const target = getCorrectedDay(day.time * 1000).toISOString().substring(0, 10);
+    let match;
+    for (let sunday of sundays) {
+        if (new Date(sunday.date).toISOString().substring(0, 10) === target) {
+            match = sunday;
+            break;
+        }
+    }
+    return match ? (match.open ? "shops-open" : "shops-closed") : "shops-undefined";
+}
+
+function isSunday(day) {
+    return getCorrectedDay(day.time * 1000).getDay() === 0;
+}
+
+function getCorrectedDay(day) {
+    return new Date(new Date(day).getTime() - (new Date(day).getTimezoneOffset() * 60000));
+}
+
+function timestampToShortDate(timestamp) {
+    const date = getCorrectedDay(new Date(timestamp * 1000));
+    const months = [ "Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Paź", "Lis", "Gru" ];
+    return date.getDate() + " " + months[date.getMonth()];
+}
+
+function timestampToDayName(timestamp) {
+    let output = "";
+    const day = getCorrectedDay(new Date(timestamp * 1000));
+    const now = getCorrectedDay(new Date());
+    if (now.toISOString().slice(0,10) === day.toISOString().slice(0,10))
+        output += "Dzisiaj – ";
+
+    let names = [];
+    names[0] = "Niedziela"; names[1] = "Poniedziałek"; names[2] = "Wtorek"; names[3] = "Środa";
+    names[4] = "Czwartek"; names[5] = "Piątek"; names[6] = "Sobota";
+    return output + names[day.getDay()];
+}
+
+function getColor(day) {
+    let colors = {};
+    colors["clear-day"] = "orange";
+    colors["clear-night"] = "blue";
+    colors["rain"] = "cyan";
+    colors["snow"] = "silver";
+    colors["sleet"] = "teal";
+    colors["wind"] = "red";
+    colors["fog"] = "gray";
+    colors["cloudy"] = "#666";
+    colors["partly-cloudy-day"] = "yellow";
+    colors["partly-cloudy-night"] = "green";
+
+    return colors[day.icon];
+}
+
+function generateIcon(day, size=64) {
+    let icon = document.createElement("canvas");
+    icon.width = icon.height = size;
+    let skycons = new Skycons({ "color": "#1e5799" });
+    skycons.add(icon, day.icon);
+    skycons.play();
+    return icon;
+}
+
+function generateSummary(day, summaryType="daily") {
+    let summary = document.createElement("span");
+    summary.classList.add("summary");
+    summary.classList.add(summaryType);
+    //summary.innerHTML = day.summary.replace(/(.*)\./, "$1");
+    summary.innerHTML = day.summary;
+    return summary;
+}
+
+function generateTemperatureBar(day, temperatureRange) {
+    const rangeMin = temperatureRange[0];
+    const tempMin = Math.round(day.temperatureMin);
+    const tempMax = Math.round(day.temperatureMax);
+
+    let bar = document.createElement("span");
+    bar.classList.add("bar");
+    if (tempMax - tempMin <= 1)
+        bar.classList.add("min-equals-max");
+    bar.setAttribute("temp-min", tempMin);
+    bar.setAttribute("temp-max", tempMax);
+    const columnStart = 1 + tempMin - rangeMin;
+    const columnEnd = columnStart + tempMax - tempMin;
+    bar.style.gridArea = `1 / ${columnStart} / -1 / ${columnEnd}`;
+    
+    let bg = document.createElement("span");
+    bg.classList.add("bg");
+
+    let container = document.createElement("div");
+    container.classList.add("temperature-range");
+    container.appendChild(bar);
+    container.appendChild(bg);
+
+    return container;
+}
+
+function appendWeatherBox(day, tempMinMax) {
+    let box = document.createElement("div");
+    box.classList.add("info-box");
+    if (isSunday(day))
+        box.classList.add(getClassForSunday(day));
+    box.appendChild(generateIcon(day, 64));
+    box.appendChild(generateSummary(day));
+    box.appendChild(generateTemperatureBar(day, tempMinMax));
+    box.setAttribute("day-of-week", timestampToDayName(day.time));
+    box.setAttribute("day-of-month", timestampToShortDate(day.time));
+    document.body.appendChild(box);
+}
+
+function getTemperatureRange(daily) {
+    let min = 1000;
+    let max = -1000;
+    for (let day of daily) {
+        if (day.temperatureMin < min) min = day.temperatureMin;
+        if (day.temperatureMax > max) max = day.temperatureMax;
+    }
+    return [ Math.round(min), Math.round(max) ];
+}
+
+function getAirlyMeasurements() {
+    const level = airly.current.indexes[0].level.replace(/_/g, "-").toLowerCase();
+    const value = airly.current.indexes[0].value;
+    const pm25percent = airly.current.standards.filter(x => x.pollutant === "PM25")[0].percent;
+    const pm10percent = airly.current.standards.filter(x => x.pollutant === "PM10")[0].percent;
+    return [ level, value, Math.round(pm25percent), Math.round(pm10percent) ];
+}
+
+function appendPollutantBar(rootNode, pollutant, percentage) {
+    let name = document.createElement("span");
+    name.classList.add("name");
+    name.classList.add("pollutant");
+    name.setAttribute("pollutant", pollutant);
+    name.innerHTML = pollutant;
+
+    let fill = document.createElement("span");
+    fill.classList.add("bar");
+    fill.classList.add("pollutant");
+    fill.setAttribute("pollutant", pollutant);
+    fill.style.width = Math.min(percentage, 100) + "%";
+
+    let bg = document.createElement("span");
+    bg.classList.add("bg");
+    bg.classList.add("pollutant");
+    bg.setAttribute("pollutant", pollutant);
+
+    let value = document.createElement("span");
+    value.classList.add("percentage");
+    value.classList.add("pollutant");
+    value.setAttribute("pollutant", pollutant);
+    value.innerHTML = percentage + "%";
+
+    rootNode.appendChild(name);
+    rootNode.appendChild(fill);
+    rootNode.appendChild(bg);
+    rootNode.appendChild(value);
+}
+
+function generateAir() {
+    const [ level, value, pm25percent, pm10percent ] = getAirlyMeasurements();
+    
+    let air = document.createElement("div");
+    air.classList.add("air-quality");
+    air.classList.add(level);
+    
+    let index = document.createElement("span");
+    index.classList.add("value");
+    index.innerHTML = Math.round(value);
+    
+    air.appendChild(index);
+    appendPollutantBar(air, "PM2.5", pm25percent);
+    appendPollutantBar(air, "PM10", pm10percent);
+
+    return air;
+}
+
+function generateHourlyBar(hourly) {
+    const maxRange = Math.min(24, hourly.data.length)
+    let bar = document.createElement("div");
+    bar.classList.add("hourly-bar");
+    bar.style.gridTemplateColumns = `repeat(${maxRange}, 1fr)`;
+    
+    let previousStatus = "";
+    let previousEnd = 0;
+    for (let index = 0; index <= maxRange; index++) {
+        const hour = hourly.data[index];
+        let start;
+        let end;
+        const status = hour ? hour.icon.replace(/\-(night|day)/g, "") : "";
+
+        if ((previousStatus && (previousStatus != status)) || (index === maxRange)) {
+            start = previousEnd + 1;
+            end = index + 1;
+
+            let block = document.createElement("div");
+            block.classList.add(previousStatus);
+            block.style.gridColumn = `${start} / ${end}`;
+            bar.appendChild(block);
+
+            previousEnd = index;
+        }
+        previousStatus = status;
+    }
+    
+    return bar;
+}
+
+function generateCurrentWeather() {
+    let box = document.createElement("div");
+    box.classList.add("weather");
+
+    let icon = generateIcon(darksky.currently, 128);
+
+    let temperature = document.createElement("span");
+    temperature.classList.add("temperature");
+    let temperatureValue = document.createElement("span");
+    temperatureValue.innerHTML = Math.round(darksky.currently.temperature);
+    temperature.appendChild(temperatureValue);
+
+    let summaryNow = generateSummary(darksky.currently, "currently");
+    let summaryHourly = generateSummary(darksky.hourly, "hourly");
+    
+    let hourlyBar = generateHourlyBar(darksky.hourly);
+
+    box.appendChild(icon);
+    box.appendChild(temperature);
+    box.appendChild(summaryNow);
+    box.appendChild(summaryHourly);
+    box.appendChild(hourlyBar);
+
+    return box;
+}
+
+function generateMainInfo() {
+    let container = document.createElement("div");
+    container.classList.add("info-box");
+    container.classList.add("current");
+    container.setAttribute("day-of-week", "teraz");
+    container.appendChild(generateCurrentWeather());
+    container.appendChild(generateAir());
+    document.body.appendChild(container);
+}
+
+function generateWeeklySummary() { // !!!
+}
+
+function generateDailyWeather() {
+    const daily = darksky.daily.data;
+    const tempMinMax = getTemperatureRange(daily);
+    for (let day of daily) {
+        appendWeatherBox(day, tempMinMax);
+    }
+    const range = tempMinMax[1] - tempMinMax[0];
+    applyCss(`.temperature-range { grid-template-columns: repeat(${range}, 1fr); }`);
+}
+
+function generateFooter() { // !!!
+}
+
+function init() {
+    generateMainInfo();
+    generateWeeklySummary();
+    generateDailyWeather();
+    generateFooter();
+}
+
+if (document.readyState != "loading")
+    init();
+else if (document.addEventListener)
+    document.addEventListener("DOMContentLoaded", init);
+else
+    document.attachEvent("onreadystatechange", function() {
+        if (document.readyState == "complete")
+            init();
+    });
