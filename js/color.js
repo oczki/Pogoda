db1c862f7c11c718e66ce237f4da3adcd998efe2
fri <tehoko@gmail.com>
2019-11-25 02:24:38 +0100

    code cleanup

--- js/color.js
+++ js/color.js
@@ -1,55 +1,40 @@
 class Color {
     constructor(hex = "#000000") {
         const rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        this._r = parseInt(rgb[1], 16);
-        this._g = parseInt(rgb[2], 16);
-        this._b = parseInt(rgb[3], 16);
+        this.r = parseInt(rgb[1], 16);
+        this.g = parseInt(rgb[2], 16);
+        this.b = parseInt(rgb[3], 16);
     }
-    
-    set r(r) { this._r = r; }
-    set g(g) { this._g = g; }
-    set b(b) { this._b = b; }
-    get r() { return this._r; }
-    get g() { return this._g; }
-    get b() { return this._b; }
-    
+
     get rgb() {
-        return [ this._r, this._g, this._b ];
+        return [this.r, this.g, this.b];
     }
-    
+
     get hex() {
-        return "#" + ((1 << 24) + (this._r << 16) + (this._g << 8) + this._b
-                     ).toString(16).slice(1);
+        return "#" + ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b)
+            .toString(16).slice(1);
     }
 }
 
-class ColorMixer {
-    constructor(color1, color2, factor) {
-        this.color1 = color1;
-        this.color2 = color2;
-        this.factor = factor;
-    }
-    
-    lerp(val1, val2) {
-        const result = (1 - this.factor) * val1 + this.factor * val2;
+function mixColors(color1, color2, factor = 0.5) {
+    function lerp(val1, val2) {
+        const result = (1 - factor) * val1 + factor * val2;
         return Math.round(result);
     }
-    
-    mix() {
-        let mixed = new Color();
-        mixed.r = this.lerp(this.color1.r, this.color2.r);
-        mixed.g = this.lerp(this.color1.g, this.color2.g);
-        mixed.b = this.lerp(this.color1.b, this.color2.b);
-        return mixed;
-    }
+
+    let mixed = new Color();
+    mixed.r = lerp(color1.r, color2.r);
+    mixed.g = lerp(color1.g, color2.g);
+    mixed.b = lerp(color1.b, color2.b);
+    return mixed;
 }
 
-function darken(input, factor=0.5) {
+function darken(input, factor = 0.5) {
     const black = new Color("#000000");
-    return new ColorMixer(input, black, factor).mix();
+    return mixColors(input, black, factor);
 }
 
-function lighten(input, factor=0.5) {
+function lighten(input, factor = 0.5) {
     const white = new Color("#ffffff");
-    return new ColorMixer(input, white, factor).mix();
+    return mixColors(input, white, factor);
 }
--- js/value-colorization.js
+++ js/value-colorization.js
@@ -4,7 +4,7 @@ class ValueColorant {
         this.minValue = Math.min(...this.lut.keys());
         this.maxValue = Math.max(...this.lut.keys());
     }
-    
+
     getBoundsAndFactor(input) {
         let lowerBound = this.minValue;
         for (let value = input; value >= this.minValue; value--)
@@ -19,44 +19,44 @@ class ValueColorant {
                 upperBound = value;
                 break;
             }
-        
+
         const factor = (input - lowerBound) / (upperBound - lowerBound);
-        return [ lowerBound, upperBound, factor ];
+        return [lowerBound, upperBound, factor];
     }
-    
+
     colorOf(value) {
         if (value <= this.minValue)
             return new Color(this.lut.get(this.minValue));
         if (value >= this.maxValue)
             return new Color(this.lut.get(this.maxValue));
-        
-        const [ minBound, maxBound, factor ] = this.getBoundsAndFactor(value);
+
+        const [minBound, maxBound, factor] = this.getBoundsAndFactor(value);
         const minColor = new Color(this.lut.get(minBound));
         const maxColor = new Color(this.lut.get(maxBound));
-        return new ColorMixer(minColor, maxColor, factor).mix();
+        return mixColors(minColor, maxColor, factor);
     }
 }
 
 class TemperatureColorant {
     constructor() {
         this.lut = new Map([
-            [ -30, "#1C0A8A" ],
-            [ -20, "#2F2BBD" ],
-            [ -10, "#0684BE" ],
-            [ 0  , "#14B39D" ],
-            [ 10 , "#75B340" ],
-            [ 20 , "#B9A700" ],
-            [ 30 , "#BB4400" ],
-            [ 40 , "#C30015" ],
-            [ 50 , "#9E0061" ]
+            [-30, "#1C0A8A"],
+            [-20, "#2F2BBD"],
+            [-10, "#0684BE"],
+            [  0, "#14B39D"],
+            [ 10, "#75B340"],
+            [ 20, "#B9A700"],
+            [ 30, "#BB4400"],
+            [ 40, "#C30015"],
+            [ 50, "#9E0061"]
         ]);
         this.colorant = new ValueColorant(this.lut);
     }
-    
+
     colorOf(temperature) {
         return this.colorant.colorOf(temperature);
     }
-    
+
     getTensBetween(min, max) {
         let res = [];
         if (max - min <= 1)
@@ -64,15 +64,20 @@ class TemperatureColorant {
         for (let value = min + 1; value < max; value++)
             if (value % 10 === 0) {
                 const percent = 100 * (value - min) / (max - min);
-                res.push({ value, percent });
+                res.push({
+                    value,
+                    percent
+                });
             }
         return res;
     }
-    
+
     gradient(lowTemp, highTemp) {
-        let res = `linear-gradient(to right, ${this.colorOf(lowTemp).hex} 0, `;
+        let res =
+            `linear-gradient(to right, ${this.colorOf(lowTemp).hex} 0, `;
         for (let tenStop of this.getTensBetween(lowTemp, highTemp))
-            res += `${this.colorOf(tenStop.value).hex} ${tenStop.percent}%, `;
+            res +=
+            `${this.colorOf(tenStop.value).hex} ${tenStop.percent}%, `;
         res += `${this.colorOf(highTemp).hex} 100%)`;
         return res;
     }
@@ -81,19 +86,19 @@ class TemperatureColorant {
 class AirPollutionColorant {
     constructor() {
         this.lut = new Map([
-            [ 5  , "#399323" ],
-            [ 20,  "#669818" ],
-            [ 35 , "#949E0C" ],
-            [ 50 , "#C1970B" ],
-            [ 65 , "#D16623" ],
-            [ 80 , "#D62A2A" ],
-            [ 100, "#A50052" ],
-            [ 125, "#6D006D" ],
-            [ 160, "#000000" ]
+            [  5, "#399323"],
+            [ 20, "#669818"],
+            [ 35, "#949E0C"],
+            [ 50, "#C1970B"],
+            [ 65, "#D16623"],
+            [ 80, "#D62A2A"],
+            [100, "#A50052"],
+            [125, "#6D006D"],
+            [160, "#000000"]
         ]);
         this.colorant = new ValueColorant(this.lut);
     }
-    
+
     colorOf(caqi) {
         const val = Math.round(caqi);
         return this.colorant.colorOf(val);
